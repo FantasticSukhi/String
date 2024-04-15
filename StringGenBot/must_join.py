200cfa5e87df406266cce5e751d4ee8752c52c2d
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2024-04-15 23:57:53 +0530

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -20,11 +20,11 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://te.legra.ph/file/faae9a69a1e865c6acbb1.jpg", caption=f"» 𝐅𝐈𝐑𝐒𝐓𝐋𝐘 𝐘𝐎𝐔 𝐍𝐄𝐄𝐃 𝐓𝐎 𝐉𝐎𝐈𝐍 𝐎𝐔𝐑 𝐅𝐀𝐌𝐈𝐋𝐘 𝐓𝐇𝐄𝐍 𝐘𝐎𝐔 𝐂𝐀𝐍 𝐔𝐒𝐄 𝐌𝐄 [𝐎𝐅𝐅𝐈𝐂𝐄]({link}). 𝐀𝐅𝐓𝐄𝐑 𝐉𝐎𝐈𝐍 𝐒𝐓𝐀𝐑𝐓 𝐌𝐄 𝐀𝐆𝐀𝐈𝐍 !",
+                    photo="https://te.legra.ph/file/1845a75449bbbc2e8f9f7.jpg", caption=f"» ƒιяѕтℓу уσυ ηєє∂ тσ נσιη συя ƒαмιℓу тнєη уσυ ¢αη υѕє мє. [ᴏꜰꜰɪᴄᴇ]({link}). ǟʄȶɛʀ ʝօɨռ ֆȶǟʀȶ ʍɛ ǟɢǟɨռ. !",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("</> 𝐎𝐅𝐅𝐈𝐂𝐄", url=link),
+                                InlineKeyboardButton("</> ᴏꜰꜰɪᴄᴇ", url=link),
                             ]
                         ]
                     )
